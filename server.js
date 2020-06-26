b13c5929fdcff520a5ae5521e3bd01986cc86974
errily <erril.98@gmail.com>
2020-06-26 11:14:42 +0700

    update

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 var corsOptions = {
   // origin: "https://article-irfan.herokuapp.com",
-  origin :"http://stupendous-goldfish.surge.sh"
+  origin :"holistic-taste.surge.sh"
 };
 
 
